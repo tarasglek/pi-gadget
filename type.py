195a9aa33219c3af81e16a0975d52f66097d2005
Your Name <you@example.com>
2020-07-11 06:50:57 +0100

    wip

--- type.py
+++ type.py
@@ -1,39 +1,65 @@
 #!/usr/bin/env python3
+
+import click
+
 NULL_CHAR = chr(0)
 # https://randomnerdtutorials.com/raspberry-pi-zero-usb-keyboard-hid/
+# https://stackoverflow.com/questions/22753160/how-do-i-accept-input-from-arrow-keys-or-accept-directional-input
 def write_report(report):
     with open('/dev/hidg0', 'rb+') as fd:
         fd.write(report.encode())
+"""
+https://www.usb.org/sites/default/files/documents/hut1_12v2.pdf
+"""
+def type_key(key, fd):
+    letter = lambda c: chr(ord(c) - ord('a') + 4) + NULL_CHAR * 5
+    table = {
+        ' ': 44,
+        'ENTER': 40,
+        'ESC': 41,
+        'BACKSPACE': 42,
+        'TAB': 43,
+        'F12': 69,
+        'RIGHT': 79,
+        'LEFT': 80,
+        'DOWN': 81,
+        'UP': 82
+    }
+    if key in table:
+        out = NULL_CHAR*2+chr(table[key])+NULL_CHAR*5
+    elif key >= 'a' and key <= 'z':
+        # 4 == 'a' in usb hid table
+        out = NULL_CHAR*2 + letter(key)
+    elif key >= 'A' and key <= 'Z':
+        # 4 == 'a' in usb hid table
+        out = chr(32)+NULL_CHAR + letter(key.lower())
+    else:
+        raise Exception("Unhandled key:" + key)
+    # Release all keys
+    out+=(NULL_CHAR*8)
+    fd.write(out.encode())
 
-# Press a
-write_report(NULL_CHAR*2+chr(4)+NULL_CHAR*5)
-# Release keys
-write_report(NULL_CHAR*8)
-# Press SHIFT + a = A
-write_report(chr(32)+NULL_CHAR+chr(4)+NULL_CHAR*5)
+def typeit(str):
+    with open('/dev/hidg0', 'rb+') as fd:
+        [type_key(c, fd) for c in str]
+        for word in ["ENTER", "BACKSPACE","ESC",  "F12", "TAB", "UP", "DOWN", "LEFT", "RIGHT"]:
+            type_key(word, fd)
+    # write_report(NULL_CHAR*8)
+        # type_key(None, fd)
+printable = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
 
-# Press b
-write_report(NULL_CHAR*2+chr(5)+NULL_CHAR*5)
-# Release keys
-write_report(NULL_CHAR*8)
-# Press SHIFT + b = B
-write_report(chr(32)+NULL_CHAR+chr(5)+NULL_CHAR*5)
-
-# Press SPACE key
-write_report(NULL_CHAR*2+chr(44)+NULL_CHAR*5)
-
-# Press c key
-write_report(NULL_CHAR*2+chr(6)+NULL_CHAR*5)
-# Press d key
-write_report(NULL_CHAR*2+chr(7)+NULL_CHAR*5)
-
-# Press RETURN/ENTER key
-write_report(NULL_CHAR*2+chr(40)+NULL_CHAR*5)
-
-# Press e key
-write_report(NULL_CHAR*2+chr(8)+NULL_CHAR*5)
-# Press f key
-write_report(NULL_CHAR*2+chr(9)+NULL_CHAR*5)
-
-# Release all keys
-write_report(NULL_CHAR*8)
+while True:
+    c = click.getchar()
+    click.echo()
+    if c == 'y':
+        click.echo('We will go on')
+    elif c == 'n':
+        click.echo('Abort!')
+        break
+    elif c == '\x1b[D':
+        click.echo('Left arrow <-')
+    elif c == '\x1b[C':
+        click.echo('Right arrow ->')
+    else:
+        click.echo(repr([c, ord(c)]))
+        click.echo('You pressed: "' + ''.join([ '\\'+hex(ord(i))[1:] if i not in printable else i for i in c ]) +'"' )
